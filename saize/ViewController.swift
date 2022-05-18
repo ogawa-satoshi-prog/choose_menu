3ef804c6e5dbdc327ed2b78857aee8d7e0e4bf4b
ash <ogawa.sato01@gmail.com>
2022-05-18 23:07:42 +0900

    Initial Commit

---
+++ saize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  saize
+//
+//  Created by 小川慧 on 2022/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
